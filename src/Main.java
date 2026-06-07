adadd7b365304bf989f58d0f75b34f093968625c
harikrishna <33906523+hari926@users.noreply.github.com>
2026-06-07 12:23:48 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        while(true) {
-            System.out.println("Running...");
-            Thread.sleep(5000);
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Springboot/Java container started...");
+
+        while (true) {
+            System.out.println("App is running... " + java.time.LocalDateTime.now());
+            Thread.sleep(5000); // keep container alive
         }
     }
 }
