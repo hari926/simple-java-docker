6270c4d4a937ba90df177dc402b0b0d23cd6372b
harikrishna <33906523+hari926@users.noreply.github.com>
2026-06-07 12:22:30 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import java.util.Date;
-
 public class Main {
-    public static void main(String[] args) {
-        Date currentDate = new Date();
-        System.out.println("Hello, Docker! Current date: " + currentDate);
+    public static void main(String[] args) throws Exception {
+        while(true) {
+            System.out.println("Running...");
+            Thread.sleep(5000);
+        }
     }
 }
